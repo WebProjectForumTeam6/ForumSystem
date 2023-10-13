3e365a27ffdbbf6f05377648f0b4b9e2dffa9539
Dimitar <Dimitarit1627@gmail.com>
2023-10-13 14:32:55 +0300

    Commit

---
+++ src/test/java/com/example/forummanagementsystem/ForumManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forummanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
